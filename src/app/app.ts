523cbb78a67e54e8edefd14fd718ad6f200aad57
Ciro Pizza <ciropizza2002@gmail.com>
2026-07-22 19:11:19 +0200

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,14 @@
 
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+}
