a97722a37e11f1a6faaa77b3cc310b77d167cde6
Ciro Pizza <ciropizza2002@gmail.com>
2026-07-22 19:18:32 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,1 +1,23 @@
 
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { authInterceptor } from './_interceptors/auth/auth.interceptor';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(withFetch(),
+                      withInterceptors([authInterceptor])),
+    provideZoneChangeDetection(),
+    provideToastr({ progressBar: true,
+                    newestOnTop: true }),
+    provideAnimations()
+  ]
+};
