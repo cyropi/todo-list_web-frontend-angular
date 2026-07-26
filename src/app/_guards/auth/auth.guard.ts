a9799cbe3c00a2b284dcf83bb2b2a16af77997ff
Ciro Pizza <ciropizza2002@gmail.com>
2026-07-26 19:42:27 +0200

    Update auth.guard.ts

--- src/app/_guards/auth/auth.guard.ts
+++ src/app/_guards/auth/auth.guard.ts
@@ -1,1 +1,18 @@
 
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../_services/auth/auth.service';
+import { ToastrService } from 'ngx-toastr';
+
+export const authGuard: CanActivateFn = (route, state) => 
+{
+    const authService = inject(AuthService);
+    const toastrService = inject(ToastrService);
+    const router = inject(Router);
+    
+    if (authService.isUserAuthenticated())
+        return true;
+
+    toastrService.warning("Please, login to access this feature", "Unauthorized!");
+    return router.parseUrl("login");
+};
