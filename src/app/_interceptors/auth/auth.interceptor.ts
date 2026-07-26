7dd6cdf8e58f5c2d4a86b601bd9bdef7ad5360ea
Ciro Pizza <ciropizza2002@gmail.com>
2026-07-26 19:44:21 +0200

    Update auth.interceptor.ts

--- src/app/_interceptors/auth/auth.interceptor.ts
+++ src/app/_interceptors/auth/auth.interceptor.ts
@@ -1,1 +1,23 @@
 
+import { HttpEvent, HttpHandlerFn, HttpHeaders, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { AuthService } from '../../_services/auth/auth.service';
+
+
+export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>>
+{
+    const authService = inject(AuthService);
+    const token = authService.getToken();
+
+    if (token)
+    {
+        req = req.clone({
+                            setHeaders: {
+                                            Authorization: 'Bearer ' + token
+                                        }
+                        });
+    }
+
+    return next(req);
+}
