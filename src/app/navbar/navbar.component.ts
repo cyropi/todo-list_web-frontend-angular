1f78120293be742856c25ca4abe72e65146802e0
Ciro Pizza <ciropizza2002@gmail.com>
2026-08-04 22:01:26 +0200

    Update navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,1 +1,29 @@
 
+import { Component, inject } from '@angular/core';
+import { DarkModeToggleComponent } from './dark-mode-toggle/dark-mode-toggle.component';
+import { AuthService } from '../_services/auth/auth.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive, DarkModeToggleComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent 
+{
+    isOpen = false;
+    authService = inject(AuthService);
+
+
+    toggle()
+    {
+        this.isOpen = !this.isOpen;
+    }
+
+
+    handleNavigationClick()
+    {
+        this.isOpen = false;
+    }
+}
