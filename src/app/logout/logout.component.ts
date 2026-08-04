25cc6313e0625e0bc8e07d2b97eaa01accbeec61
Ciro Pizza <ciropizza2002@gmail.com>
2026-08-04 07:06:56 +0200

    Update logout.component.ts

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,1 +1,35 @@
 
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../_services/auth/auth.service';
+
+@Component({
+  selector: 'app-logout',
+  imports: [],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.scss',
+})
+export class LogoutComponent 
+{
+    toastrService = inject(ToastrService);
+    router = inject(Router);
+    authService = inject(AuthService);
+
+
+    ngOnInit()
+    {
+        if (!this.authService.isAuthenticated())
+        {
+            this.toastrService.warning("You are not currently logged in!");
+            this.router.navigateByUrl("/"); 
+        }
+        else
+        {
+            this.toastrService.warning(`Come back soon, ${this.authService.user()}!`, 
+                                       "You have been logged out");
+            this.authService.logout();
+            this.router.navigateByUrl("/");
+        }
+    }
+}
