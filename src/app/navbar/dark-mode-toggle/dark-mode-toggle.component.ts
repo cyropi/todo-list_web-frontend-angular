36a3f30400bbcf149f9839d10e412bcf3bcf49af
Ciro Pizza <ciropizza2002@gmail.com>
2026-08-11 22:50:07 +0200

    Update dark-mode-toggle.component.ts

--- src/app/navbar/dark-mode-toggle/dark-mode-toggle.component.ts
+++ src/app/navbar/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -1,1 +1,63 @@
 
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dark-mode-toggle',
+  imports: [],
+  templateUrl: './dark-mode-toggle.component.html',
+  styleUrl: './dark-mode-toggle.component.scss',
+})
+export class DarkModeToggleComponent 
+{
+    ngOnInit()
+    {
+        const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon') as HTMLElement;
+        const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon') as HTMLElement;
+
+        if (localStorage.getItem('color-theme') === 'dark' || 
+            (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
+        {
+            themeToggleLightIcon.classList.remove('hidden');
+            document.documentElement.classList.add('dark');
+        }
+        else
+            themeToggleDarkIcon.classList.remove('hidden');
+    }
+
+
+    toggle()
+    {
+        const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon') as HTMLElement;
+        const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon') as HTMLElement;  
+
+        themeToggleDarkIcon.classList.toggle('hidden');
+        themeToggleLightIcon.classList.toggle('hidden');
+
+        if (localStorage.getItem('color-theme')) 
+        {
+            if (localStorage.getItem('color-theme') === 'light') 
+            {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            } 
+            else 
+            {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            }
+        } 
+        else 
+        {
+            if (document.documentElement.classList.contains('dark')) 
+            {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            } 
+            else 
+            {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            }
+        }
+    }
+}
