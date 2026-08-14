756cebc7cad3a0ce5b2fcaceb5b1345e15031e3b
Ciro Pizza <ciropizza2002@gmail.com>
2026-08-14 23:43:41 +0200

    Update reset-page.component.ts

--- src/app/reset-page/reset-page.component.ts
+++ src/app/reset-page/reset-page.component.ts
@@ -1,1 +1,10 @@
 
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-page',
+  imports: [],
+  templateUrl: './reset-page.component.html',
+  styleUrl: './reset-page.component.scss',
+})
+export class ResetPageComponent {}
